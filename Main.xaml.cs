a21fc7b5c85577e11cca75fdcf4b056818bd580a
Your Name <you@example.com>
2024-01-12 03:00:24 -0600

    🚀 Initial commit: Project liftoff! 🌌

---
+++ Main.xaml.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace Autoclicker
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main
+    {
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(
+            int dwFlags,
+            int dx,
+            int dy,
+            int dwData,
+            int dwExtraInfo
+        );
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        private Key _key = Key.E;
+        private bool _isRunning;
+        private readonly DispatcherTimer _assignedValue = new DispatcherTimer();
+        private bool textBoxClicked = false;
+
+        public Main()
+        {
+            InitializeComponent();
+            LoadSettings();
+            _assignedValue.Tick += AssignedValueTick;
+            _assignedValue.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            _assignedValue.Start();
+        }
+
+        private void AssignedValueTick(object sender, EventArgs e)
+        {
+            SelectedValue.Content = Math.Floor(ValueSlider.Value);
+            SaveSettings();
+        }
+
+        private void MoveApp(object sender, MouseButtonEventArgs e) => DragMove();
+
+        private void CloseApp(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void MinimizeApp(object sender, RoutedEventArgs e) =>
+            WindowState = WindowState.Minimized;
+
+        private void kebind_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var textbox = (TextBox)sender;
+
+            if (!textbox.IsKeyboardFocusWithin)
+            {
+                textbox.Clear();
+                textbox.Focus();
+            }
+
+            textBoxClicked = true;
+        }
+
+        private void kebind_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var textbox = (TextBox)sender;
+
+            if (!textBoxClicked || !char.IsLetter(e.Key.ToString(), 0))
+                return;
+
+            var newText = e.Key.ToString();
+            Enum.TryParse(newText, out _key);
+            textbox.Text = newText;
+            e.Handled = true;
+
+            textBoxClicked = false;
+        }
+
+        private void MakeAppTopmost(object sender, MouseButtonEventArgs e)
+        {
+            if (Topmost)
+            {
+                Topmost = false;
+            }
+            else
+            {
+                Topmost = true;
+            }
+        }
+
+        private async void StartAc(object sender, RoutedEventArgs e)
+        {
+            _isRunning = !_isRunning;
+            StartButton.Content = _isRunning ? "Stop" : "Start";
+            SetStatus(!_isRunning ? "Stopped" : null, true);
+            SetStatus(!_isRunning ? "Start AC" : null);
+
+            var hWnd = FindWindow(null, "Roblox");
+
+            while (true)
+            {
+                if (!_isRunning)
+                    break;
+
+                var isKeyPressed = GetAsyncKeyState(KeyInterop.VirtualKeyFromKey(_key)) != 0;
+                var foregroundWindow = GetForegroundWindow();
+
+                SetStatus(foregroundWindow != hWnd ? "Go to Roblox" : "Waiting key");
+
+                SetStatus(
+                    foregroundWindow != hWnd
+                        ? "Started"
+                        : isKeyPressed
+                            ? "Clicking"
+                            : "idle",
+                    true
+                );
+                SetStatus(
+                    foregroundWindow != hWnd
+                        ? "Open Roblox"
+                        : isKeyPressed
+                            ? "Enjoy"
+                            : "Waiting key"
+                );
+                if (foregroundWindow == hWnd)
+                {
+                    ((Storyboard)TryFindResource("UnFocus")).Begin();
+                    Opacity = 0.5;
+                }
+                else
+                {
+                    ((Storyboard)TryFindResource("OnFocus")).Begin();
+
+                    Opacity = 1;
+                }
+                if (foregroundWindow == hWnd && isKeyPressed)
+                {
+                    SimulateMouseClick();
+                    Thread.Sleep((int)(1000.0 / ValueSlider.Value));
+                }
+
+                await Task.Delay(10);
+            }
+        }
+
+        private static void SimulateMouseClick()
+        {
+            mouse_event(0x00000002, 0, 0, 0, 0);
+            mouse_event(0x00000004, 0, 0, 0, 0);
+        }
+
+        private void SetStatus(string input, [Optional] bool indication)
+        {
+            if (!indication)
+                Indication.Content = "Indication: " + input;
+            else
+                Status.Content = $"Status: {input}";
+        }
+
+        private void SaveSettings()
+        {
+            Properties.Settings.Default.Keybind = _key.ToString();
+            Properties.Settings.Default.CPSValue = ValueSlider.Value;
+            Properties.Settings.Default.Topmost = Topmost;
+            Properties.Settings.Default.Save();
+        }
+
+        private void LoadSettings()
+        {
+            var k = new KeyConverter();
+            Keybind.Text = Properties.Settings.Default.Keybind;
+            _key = (Key)k.ConvertFromString(Properties.Settings.Default.Keybind);
+            ValueSlider.Value = Properties.Settings.Default.CPSValue;
+            Topmost = Properties.Settings.Default.Topmost;
+            topmost_checkbox.IsChecked = Topmost;
+        }
+
+        private void ToggleTopmost(object sender, RoutedEventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            Topmost = checkbox.IsChecked == true ? true : false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,59 @@
+# Roblox Autoclicker🖱️
+
+Roblox Autoclicker is a feature-rich autoclicker designed for Roblox, developed in C# using .NET Framework and WPF. It provides a seamless experience for automating repetitive tasks in the game while introducing additional features to enhance user control.
+
+## Features ✨
+
+- **Click Rate Limiter**: Set a maximum number of clicks per second to avoid detection and ensure a safer experience.
+- **Custom Keybinds**: Define personalized keybindings to start, stop, or toggle the autoclicker on demand.
+- **Automatic Game Detection**: Roblox Autoclicker intelligently detects the running Roblox game, making it easy to use without manual configuration.
+- **Opacity .5 on game**: Roblox Autoclicker automatically lowers its opacity to .5 when the Roblox game window is active, allowing you to see through the autoclicker.
+- **Customizable Hotkeys**: Customize the hotkeys that control the autoclicker to your liking.
+- **Easy-to-use UI**: Roblox Autoclicker's intuitive user interface makes it easy to configure and use.
+  
+## Usage 🚀
+
+1. Download [Latest Relase](https://github.com/Kuda-Development/Roblox-Autoclicker/releases/latest)
+2. Open Roblox AC.exe 
+3. Set Keybind, CPS & Topmost Preferences
+4. Click start
+5. Go to roblox and press the custom keybind
+   
+## Preview 😺
+
+https://github.com/Kuda-Development/Roblox-Autoclicker/assets/95893893/1e035e06-479e-40b7-a28e-9506b27aebb1
+
+## Source Code Usage 🚀
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Kuda-Development/Blade-Ball-Autoclicker.git
+   ```
+
+2. Open the solution in Visual Studio.
+3. Build and run the project.
+
+4. Customize settings through the intuitive user interface.
+
+5. Enjoy automated clicking in Roblox with added features.
+
+## Coming Soon 🔮
+
+Roblox Autoclicker is actively under development, with exciting new features on the horizon. Stay tuned for upcoming updates, including:
+
+- **User Profiles**: Save and load different settings profiles for various use cases.
+
+## Contributing 🤝
+
+We welcome contributions from the community. If you have ideas, bug reports, or feature requests, please open an issue or submit a pull request.
+
+## License 📄
+
+Roblox Autoclicker is licensed under the MIT License.
+
+**Note**: Roblox Autoclicker is developed by Kuda Development and is not affiliated with or endorsed by Roblox Corporation.
+
+
+
+## KUDA LLC 😺
